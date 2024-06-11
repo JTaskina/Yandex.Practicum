17b35f8694f5fc6ecbae2ea83e0e9dd62f7ce939
JTaskina <zul79@list.ru>
2024-06-11 20:03:13 +0300

    SQL

--- SQL/Задачи/Задача 16/Задача 1.sql
+++
@@ -1,3 +0,0 @@
-SELECT COUNT(*)
-FROM company
-WHERE status = 'closed';
--- SQL/Задачи/Задача 2/Задача 2.sql
+++
@@ -1,5 +0,0 @@
-SELECT   funding_total
-FROM     company
-WHERE    category_code = 'news'
-     AND      country_code = 'USA'
-ORDER BY funding_total DESC
